33a63d6ffaff16cafc38089e369297a797841c8f
Nelson Mfinda <nelson.mfinda@nextbss.co.ao>
2019-10-21 10:31:59 -0400

    Create event list and edit components

--- src/router.js
+++ src/router.js
@@ -9,7 +9,9 @@ import RegistOrganizer from '@/views/Organizers/RegistOrganizer'
 import ListOrganizer from '@/views/Organizers/ListOrganizer'
 import EditOrganizer from '@/views/Organizers/EditOrganizer'
 
-import NewEvent from '@/views/Events/NewEvent'
+import RegistEvent from '@/views/Events/RegistEvent'
+import ListEvent from '@/views/Events/ListEvent'
+import EditEvent from '@/views/Events/EditEvent'
 
 Vue.use(Router)
 
@@ -24,55 +26,79 @@ export default new Router({
       title: 'Pagina Inicial'
     },
     children: [{
-      path: '',
-      name: 'Dashboard',
-      component: Dashboard,
-      meta: {
-        title: 'Pagina Inicial'
-      }
-    },
-    {
-      path: '/organizer',
-      name: 'ListOrganizer',
-      component: View,
-      meta: {
-        title: 'Organizadores'
-      },
-      children: [
-        {
-          path: '/',
-          name: 'ListOrganizer',
-          component: ListOrganizer,
-          meta: {
-            title: 'Organizadores'
-          }
-        },
-        {
-          path: 'regist',
-          name: 'RegistOrganizer',
-          component: RegistOrganizer,
-          meta: {
-            title: 'Novo Organizador'
-          }
-        },
-        {
-          path: 'edit/:id',
-          name: 'EditOrganizer',
-          component: EditOrganizer,
-          meta: {
-            title: 'Editar Organizador'
-          }
+        path: '',
+        name: 'Dashboard',
+        component: Dashboard,
+        meta: {
+          title: 'Pagina Inicial'
         }
-      ]
-    },
-    {
-      path: 'event',
-      name: 'NewEvent',
-      component: NewEvent,
-      meta: {
-        title: 'Novo Evento'
+      },
+      {
+        path: '/organizer',
+        name: 'ListOrganizer',
+        component: View,
+        meta: {
+          title: 'Organizadores'
+        },
+        children: [{
+            path: '/',
+            name: 'ListOrganizer',
+            component: ListOrganizer,
+            meta: {
+              title: 'Organizadores'
+            }
+          },
+          {
+            path: 'regist',
+            name: 'RegistOrganizer',
+            component: RegistOrganizer,
+            meta: {
+              title: 'Novo Organizador'
+            }
+          },
+          {
+            path: 'edit/:id',
+            name: 'EditOrganizer',
+            component: EditOrganizer,
+            meta: {
+              title: 'Editar Organizador'
+            }
+          }
+        ]
+      },
+      {
+        path: '/events',
+        name: 'ListEvent',
+        component: View,
+        meta: {
+          title: 'Listar Eventos'
+        },
+        children: [{
+            path: '/',
+            name: 'ListEvent',
+            component: ListEvent,
+            meta: {
+              title: 'Listar Eventos'
+            }
+          },
+          {
+            path: 'regist',
+            name: 'RegistEvent',
+            component: RegistEvent,
+            meta: {
+              title: 'Registar Evento'
+            }
+          },
+          {
+            path: 'edit/:id',
+            name: 'EditEvent',
+            component: EditEvent,
+            meta: {
+              title: 'Editar Evento'
+            }
+          }
+        ]
       }
-    }
     ]
   }]
 })
--- src/views/Events/RegistEvent.vue
+++ src/views/Events/RegistEvent.vue
@@ -88,26 +88,141 @@
       </h2>
 
       <div class="row">
-        <div class="form-group">
-          <label for="event-name">Nome</label>
-          <input type="text" class="form-control" id="event-name" placeholder="Nome do evento" />
+        <div class="col-md-9">
+          <table class="table table-striped">
+            <thead>
+              <tr>Sessão 1</tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Casal VIP</th>
+                <td>Quantidade</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="localization"
+                    placeholder="Ex.: 10"
+                  />
+                </td>
+                <td>Preço</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="price"
+                    placeholder="Ex.: 1000"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Solteiro VIP</th>
+                <td>Quantidade</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="localization"
+                    placeholder="Ex.: 10"
+                  />
+                </td>
+                <td>Preço</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="price"
+                    placeholder="Ex.: 1000"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Casal Normal</th>
+                <td>Quantidade</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="quatity"
+                    placeholder="Ex.: 10"
+                  />
+                </td>
+                <td>Preço</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="price"
+                    placeholder="Ex.: 1000"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <h3>Serviços Extras</h3>
+
+      <div class="row">
+        <div class="col-md-9">
+          <table class="table table-striped">
+            <tbody>
+              <tr>
+                <th scope="row">Estacionamento</th>
+                <td>Preço</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="price"
+                    placeholder="Ex.: 1000"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Lounge Bar</th>
+                <td>Preço</td>
+                <td>
+                  <input
+                    type="number"
+                    class="form-control col-md-5"
+                    id="price"
+                    placeholder="Ex.: 1000"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <button class="btn btn-primary mr-3">Anterior</button>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+
+      <h2>
+        Patrocinadores
+        <span class="badge badge-pill badge-info">3</span>
+      </h2>
+
+      <div class="row">
+        <input type="text" class="form-control col-md-5" placeholder="Ex.: Nextbss" />
+      </div>
+
+      <h3>Disponibilidade dos ingressos</h3>
+      <div class="row">
+        <div class="col-md-3">
+          <input type="date" class="form-control" id="event-date" />
+          <small class="form-text text-muted">Início de vendas</small>
+        </div>
+        <div class="col-md-3">
+          <input type="date" class="form-control" id="event-date" />
+          <small class="form-text text-muted">Fim de vendas</small>
         </div>
       </div>
 
       <button class="btn btn-primary mr-3">Anterior</button>
       <button class="btn btn-primary">Próximo</button>
-
-      <h2>
-        Serviços adicionais
-        <span class="badge badge-pill badge-info">3</span>
-      </h2>
-
-      <div class="row">
-        <div class="col-md-4"></div>
-      </div>
-
-      <button class="btn btn-primary mr-3">Anterior</button>
-      <button type="submit" class="btn btn-primary">Enviar</button>
     </div>
   </div>
 </template>
