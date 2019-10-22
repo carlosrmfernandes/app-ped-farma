7d75c1ce718eb9ac95baf1d81a623dcba0831d57
Nelson Mfinda <nelson.mfinda@nextbss.co.ao>
2019-10-22 10:11:31 -0400

    Implement Vuelidate and ImageUploader

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import ImageUploader from 'vue-image-upload-resize'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(Vuelidate)
+Vue.use(ImageUploader)
 
 new Vue({
   router,
