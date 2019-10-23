47506cedd4ed20528db2c22da4715c96c531c2f1
Aristóteles Lopes <aristoteles.lopes@nextbss.co.ao>
2019-10-23 16:26:32 +0100

    insert z-index to element

--- src/components/Layouts/Breadcrumbs.vue
+++ src/components/Layouts/Breadcrumbs.vue
@@ -7,11 +7,13 @@ export default {}
 
 <style >
 .header {
-  position: absolute;
+  position: fixed;
   width: 90%;
   height: 10%;
   top: 0;
   right: 0;
   /* border: 1px solid red; */
+  background-color: #fff;
+  z-index: 1;
 }
 </style>
