c0b18be6b35a59dba76149e240d82c8d936e6a44
Nelson Mfinda <nelson.mfinda@nextbss.co.ao>
2019-10-25 12:00:58 -0400

    Implement regist organizers step one

--- src/views/Events/RegistEvent.vue
+++ src/views/Events/RegistEvent.vue
@@ -1,85 +1,89 @@
 <template>
-  <div>
+  <div id="steps-form">
     <h1>Novo Evento</h1>
-    <h2>
-      Identificação do Evento
-      <span class="badge badge-pill badge-info">1</span>
-    </h2>
-    <hr />
-    <div>
-      <div class="row">
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-name">Nome</label>
-            <input type="text" class="form-control" id="event-name" placeholder="Nome do evento" />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-class">Localização</label>
-            <select id="location" class="form-control">
-              <option v-for="location in locations">{{ location.description }}</option>
-            </select>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="form-group">
-            <label for>Sessão</label>
-            <div class="row">
-              <div class="col-md-4">
-                <input type="text" class="form-control" id="localization" placeholder="Localização" />
-                <small class="form-text text-muted">Localização do evento</small>
-              </div>
-              <div class="col-md-3">
-                <input type="date" class="form-control" id="event-date" />
-                <small class="form-text text-muted">Data do evento</small>
-              </div>
-              <div class="col-md-4">
-                <input type="time" class="form-control" id="event-time" />
-                <small class="form-text text-muted">Hora do evento</small>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="form-group">
-            <label for>Detalhes</label>
+    <section v-if="step === 1">
+      <h2>
+        Identificação do Evento
+        <span class="badge badge-pill badge-info">1</span>
+      </h2>
+      <hr />
+      <div>
+        <div class="row">
+          <div class="col-md-3">
             <div class="form-group">
-              <textarea class="form-control" rows="4" placeholder="Descrição do evento"></textarea>
+              <label for="event-name">Nome</label>
+              <input type="text" class="form-control" id="event-name" placeholder="Nome do evento" />
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="form-group">
+              <label for="event-class">Localização</label>
+              <select id="location" class="form-control">
+                <option v-for="location in locations">{{ location.description }}</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for>Sessão</label>
+              <div class="row">
+                <div class="col-md-4">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="localization"
+                    placeholder="Localização"
+                  />
+                  <small class="form-text text-muted">Localização do evento</small>
+                </div>
+                <div class="col-md-3">
+                  <input type="date" class="form-control" id="event-date" />
+                  <small class="form-text text-muted">Data do evento</small>
+                </div>
+                <div class="col-md-4">
+                  <input type="time" class="form-control" id="event-time" />
+                  <small class="form-text text-muted">Hora do evento</small>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-name">Nome</label>
-            <input type="text" class="form-control" id="event-name" placeholder="Nome do evento" />
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <label for>Detalhes</label>
+              <div class="form-group">
+                <textarea class="form-control" rows="4" placeholder="Descrição do evento"></textarea>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 form-group mt-4">
+            <div class="custom-file">
+              <label class="custom-file-label" for="inputGroupFile01">Carregar poster</label>
+              <input type="file" class="custom-file-input" id="inputGroupFile01" />
+              <small class="form-text text-muted">
+                Anexar uma imagem no formato
+                <i>JPG, Paisagem 16/9</i>.
+              </small>
+            </div>
+          </div>
+          <div class="col-md-3 form-group mt-4">
+            <div class="custom-file">
+              <input type="file" class="custom-file-input" id="inputGroupFile01" />
+              <label class="custom-file-label" for="inputGroupFile01">Vídeo Promocional</label>
+              <small class="form-text text-muted">
+                Carregar um vídeo no formato
+                <i>MP4, com uma duração de 5 minutos.</i>.
+              </small>
+            </div>
           </div>
         </div>
-        <div class="col-md-3 form-group mt-4">
-          <div class="custom-file">
-            <label class="custom-file-label" for="inputGroupFile01">Carregar poster</label>
-            <input type="file" class="custom-file-input" id="inputGroupFile01" />
-            <small class="form-text text-muted">
-              Anexar uma imagem no formato
-              <i>JPG, Paisagem 16/9</i>.
-            </small>
-          </div>
-        </div>
-        <div class="col-md-3 form-group mt-4">
-          <div class="custom-file">
-            <input type="file" class="custom-file-input" id="inputGroupFile01" />
-            <label class="custom-file-label" for="inputGroupFile01">Vídeo Promocional</label>
-            <small class="form-text text-muted">
-              Carregar um vídeo no formato
-              <i>MP4, com uma duração de 5 minutos.</i>.
-            </small>
-          </div>
-        </div>
+        <button @click.prevent="next()" class="btn btn-primary">Próximo</button>
       </div>
-      <button class="btn btn-primary">Próximo</button>
+    </section>
+    <!-- Step one -->
 
+    <section v-if="step === 2">
       <h2>
         Serviços Adicionais no Evento
         <span class="badge badge-pill badge-info">2</span>
@@ -159,45 +163,12 @@
           </table>
         </div>
       </div>
+      <button @click.prevent="prev()" class="btn btn-primary">Anterior</button>
+      <button @click.prevent="next()" class="btn btn-primary ml-2">Próximo</button>
+    </section>
+    <!-- Step two -->
 
-      <h3>Serviços Extras</h3>
-
-      <div class="row">
-        <div class="col-md-9">
-          <table class="table table-striped">
-            <tbody>
-              <tr>
-                <th scope="row">Estacionamento</th>
-                <td>Preço</td>
-                <td>
-                  <input
-                    type="number"
-                    class="form-control col-md-5"
-                    id="price"
-                    placeholder="Ex.: 1000"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">Lounge Bar</th>
-                <td>Preço</td>
-                <td>
-                  <input
-                    type="number"
-                    class="form-control col-md-5"
-                    id="price"
-                    placeholder="Ex.: 1000"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      <button class="btn btn-primary mr-3">Anterior</button>
-      <button type="submit" class="btn btn-primary">Enviar</button>
-
+    <section v-if="step === 3">
       <h2>
         Patrocinadores
         <span class="badge badge-pill badge-info">3</span>
@@ -218,10 +189,10 @@
           <small class="form-text text-muted">Fim de vendas</small>
         </div>
       </div>
-
-      <button class="btn btn-primary mr-3">Anterior</button>
-      <button class="btn btn-primary">Próximo</button>
-    </div>
+      <button @click.prevent="prev()" class="btn btn-primary">Anterior</button>
+      <button @click.prevent="submit()" class="btn btn-primary ml-2">Finalizar</button>
+    </section>
+    <!-- Step three -->
   </div>
 </template>
 
@@ -229,7 +200,19 @@
 export default {
   data: function() {
     return {
-      locations: ""
+      step: 1,
+      stepCount: 3,
+      sessionsCount: 0,
+      locations: "",
+      event_name: "",
+      event_description: "",
+      event_backdrop: "",
+      event_poster: "",
+      event_starts_at: "",
+      event_video_id: "",
+      event_title: "",
+      event_location_id: "",
+      event_classification: ""
     };
   },
   methods: {
@@ -244,7 +227,15 @@ export default {
       } catch (e) {
         this.hadError = "Não foi possível carregar as informações.";
       }
-    }
+    },
+    async postPartyEvent(step) {},
+    next: function() {
+      this.step++;
+    },
+    prev: function() {
+      this.step--;
+    },
+    submit: function() {}
   },
   created() {
     this.getLocations();
