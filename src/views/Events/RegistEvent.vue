0defaed9cb995adb049fc9c9024524dc2ebd616a
Nelson Mfinda <nelson.mfinda@nextbss.co.ao>
2019-10-29 08:58:07 -0400

    Implement step one post method

--- src/views/Events/RegistEvent.vue
+++ src/views/Events/RegistEvent.vue
@@ -12,38 +12,40 @@
           <div class="col-md-3">
             <div class="form-group">
               <label for="event-name">Nome</label>
-              <input type="text" class="form-control" id="event-name" placeholder="Nome do evento" />
+              <input
+                type="text"
+                class="form-control"
+                id="title"
+                v-model="title"
+                placeholder="Nome do evento"
+              />
             </div>
           </div>
           <div class="col-md-3">
             <div class="form-group">
               <label for="event-class">Localização</label>
-              <select id="location" class="form-control">
-                <option v-for="location in locations">{{ location.description }}</option>
+              <select id="location" class="form-control" v-model="location_id">
+                <option
+                  v-for="location in locations"
+                  :value="location.id"
+                  :key="location.id"
+                >{{ location.description }}</option>
               </select>
             </div>
           </div>
           <div class="col-md-6">
-            <div class="form-group">
-              <label for>Sessão</label>
-              <div class="row">
-                <div class="col-md-4">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="localization"
-                    placeholder="Localização"
-                  />
-                  <small class="form-text text-muted">Localização do evento</small>
-                </div>
-                <div class="col-md-3">
-                  <input type="date" class="form-control" id="event-date" />
-                  <small class="form-text text-muted">Data do evento</small>
-                </div>
-                <div class="col-md-4">
-                  <input type="time" class="form-control" id="event-time" />
-                  <small class="form-text text-muted">Hora do evento</small>
-                </div>
+            <div class="row">
+              <div class="form-group">
+                <label for="event-class">Classificação</label>
+                <select v-model="classification" class="form-control">
+                  <option selected id="classification">A</option>
+                  <option id="classification">E</option>
+                  <option id="classification">T</option>
+                </select>
+              </div>
+              <div class="form-group ml-3 col-md-3">
+                <label for="event-class">Data do Evento</label>
+                <input type="date" class="form-control" id="startsAt" v-model="starts_at" />
               </div>
             </div>
           </div>
@@ -53,32 +55,33 @@
             <div class="form-group">
               <label for>Detalhes</label>
               <div class="form-group">
-                <textarea class="form-control" rows="4" placeholder="Descrição do evento"></textarea>
+                <textarea
+                  class="form-control"
+                  id="description"
+                  v-model="description"
+                  rows="4"
+                  placeholder="Descrição do evento"
+                ></textarea>
               </div>
             </div>
           </div>
-          <div class="col-md-3 form-group mt-4">
-            <div class="custom-file">
-              <label class="custom-file-label" for="inputGroupFile01">Carregar poster</label>
-              <input type="file" class="custom-file-input" id="inputGroupFile01" />
-              <small class="form-text text-muted">
-                Anexar uma imagem no formato
-                <i>JPG, Paisagem 16/9</i>.
-              </small>
-            </div>
+          <div class="col-md-3">
+            <UploadPhoto :OnChange="SelectImage" v-model="file" width="140px" height="185px" />
           </div>
           <div class="col-md-3 form-group mt-4">
-            <div class="custom-file">
-              <input type="file" class="custom-file-input" id="inputGroupFile01" />
-              <label class="custom-file-label" for="inputGroupFile01">Vídeo Promocional</label>
-              <small class="form-text text-muted">
-                Carregar um vídeo no formato
-                <i>MP4, com uma duração de 5 minutos.</i>.
-              </small>
+            <div class="form-group">
+              <label for="evideoId">ID do Vídeo Promocional</label>
+              <input
+                type="text"
+                class="form-control"
+                id="videoId"
+                v-model="video_id"
+                placeholder="Ex.: "
+              />
             </div>
           </div>
         </div>
-        <button @click.prevent="next()" class="btn btn-primary">Próximo</button>
+        <button @click.prevent="postPartyEvent(1)" class="btn btn-primary">Próximo</button>
       </div>
     </section>
     <!-- Step one -->
@@ -197,27 +200,32 @@
 </template>
 
 <script>
+import moment from "moment";
+import UploadPhoto from "@/components/Form/Photo";
+
 export default {
+  components: {
+    UploadPhoto
+  },
   data: function() {
     return {
       step: 1,
       stepCount: 3,
       sessionsCount: 0,
       locations: "",
-      event_name: "",
-      event_description: "",
-      event_backdrop: "",
-      event_poster: "",
-      event_starts_at: "",
-      event_video_id: "",
-      event_title: "",
-      event_location_id: "",
-      event_classification: ""
+      classifications: "",
+      file: "",
+      classification: "",
+      title: "",
+      description: "",
+      starts_at: "",
+      video_id: "",
+      location_id: ""
     };
   },
   methods: {
     /**
-     * GetEvent: This method will fire a GET request and then
+     * getLOcations: This method will fire a GET request and then
      * assign the response data into the state property: form
      */
     async getLocations() {
@@ -228,7 +236,64 @@ export default {
         this.hadError = "Não foi possível carregar as informações.";
       }
     },
-    async postPartyEvent(step) {},
+    async getClassifications() {
+      try {
+        // this.axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.TOKEN}`;
+        const result = await this.axios.get(`/party_events/classifications`);
+        this.classifications = result;
+      } catch (e) {
+        this.hadError = "Não foi possível carregar as informações.";
+      }
+    },
+    formatDate(date) {
+      return moment(date).toISOString();
+    },
+    async postPartyEvent(step) {
+      // /api/party_events/company/8cdf8e59-356e-4e09-9a96-bda0db2b12f3/step_1
+      if (step === 1) {
+        try {
+          this.starts_at = this.formatDate(this.starts_at);
+          // const result = await this.axios.post('/party_events/company/8cdf8e59-356e-4e09-9a96-bda0db2b12f3/step_1', this.form)
+          // if (result) {
+          // Create a new form data object
+          const fData = new FormData();
+          fData.append("poster", this.file);
+          fData.append("classification", this.classification);
+          fData.append("locationId", this.location_id);
+          fData.append("videoId", this.video_id);
+          fData.append("description", this.description);
+          fData.append("startsAt", this.starts_at);
+          fData.append("title", this.title);
+
+          // Fire the PUT request
+          const res = await this.axios({
+            url: `/party_events/company/8cdf8e59-356e-4e09-9a96-bda0db2b12f3/step_1`,
+            method: "post",
+            headers: { "Content-Type": "multipart/form-data" },
+            data: fData
+          });
+
+          console.log(res);
+
+          // Redirect to the ListEvent views
+          this.$router.push({ name: "ListEvent" });
+          // }
+        } catch (e) {
+          this.hadError =
+            "Não foi possível realizar esta operação. Tente novamente";
+          console.log(e);
+        }
+      } else if (step === 2) {
+      } else {
+      }
+    },
+    SelectImage(file) {
+      this.file = file;
+    },
+    setImage: function(file) {
+      this.hasImage = true;
+      this.poster = file;
+    },
     next: function() {
       this.step++;
     },
