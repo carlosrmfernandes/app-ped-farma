762fd95b571d97473188d92d7d8693302a1f04ae
Aristóteles Lopes <aristoteles.lopes@nextbss.co.ao>
2019-11-04 16:47:53 +0100

    import vee-validation

--- src/main.js
+++ src/main.js
@@ -8,16 +8,16 @@ import moment from 'moment'
 
 import PortalVue from 'portal-vue'
 import { BootstrapVue } from 'bootstrap-vue'
-import * as VeeValidate from 'vee-validate'
-// import VeeValidate, { Validator } from 'vee-validate'
+// import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
 import pt from 'vee-validate/dist/locale/pt_PT'
 
 Vue.config.productionTip = false
 
 // Set libraries
 moment.locale('pt_PT')
-VeeValidate.localize('pt_PT', pt)
 Vue.use(VeeValidate)
+Validator.localize('pt_PT', pt)
 Vue.use(VueAxios, axios)
 axios.defaults.baseURL = process.env.VUE_APP_API
 axios.defaults.headers.post['Content-Type'] = 'application/json'
