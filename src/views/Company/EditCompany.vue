4d23daf544a8cb9eb81f4f2f419229a9b7e7af91
Aristóteles Lopes <aristoteles.lopes@nextbss.co.ao>
2019-11-04 08:10:53 +0100

    now we are using edit as page

--- src/views/Company/EditCompany.vue
+++ src/views/Company/EditCompany.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="panel">
     <div class="panel-header">
-      <h1>Editar Organizador</h1>
+      <h1>Editar Empresa</h1>
       <b-button
         variant="outline-primary"
         size="sm"
         class="float-right"
-        @click="editOrganizer"
+        @click="editCompany"
         v-show="isEditable"
       >Editar</b-button>
       <b-button
         variant="success"
         size="sm"
         class="float-right"
-        @click="UpdateOrganizer"
+        @click="UpdateCompany"
         v-show="!isEditable"
       >
         <span v-if="!isRequesting">salvar</span>
@@ -40,95 +40,11 @@
       <div class="row">
         <div class="col-md-3">
           <div class="form-group">
-            <label for="event-name">Empresa</label>
-            <select class="custom-select" v-model="form.company_id" :disabled="isEditable">
-              <option selected>Choose...</option>
-              <option
-                :value="company.id"
-                v-for="(company, index) of companies"
-                :key="index"
-              >{{company.name}}</option>
-            </select>
-            <!-- <input type="text" class="form-control" v-model="form.email" id="Sponsor-name" placeholder="Nome do organizador" /> -->
+            <label for="event-name">Nome</label>
+            <input type="text" class="form-control" v-model="form.name" id="organizer-name"
+              :disabled="isEditable" placeholder="Nome do organizador" />
           </div>
         </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-name">Email</label>
-            <input
-              type="text"
-              class="form-control"
-              v-model="form.email"
-              id="Sponsor-name"
-              placeholder="Email"
-              :disabled="isEditable"
-            />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-name">Facebook</label>
-            <input
-              type="text"
-              class="form-control"
-              v-model="form.facebook_url"
-              id="Sponsor-name"
-              placeholder="URL do Facebook"
-              :disabled="isEditable"
-            />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-name">Instagram</label>
-            <input
-              type="text"
-              class="form-control"
-              v-model="form.instagram_url"
-              id="Sponsor-name"
-              placeholder="URL do Instagram"
-              :disabled="isEditable"
-            />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-name">Twitter</label>
-            <input
-              type="text"
-              class="form-control"
-              v-model="form.twitter_url"
-              id="Sponsor-name"
-              placeholder="URL do Twitter"
-              :disabled="isEditable"
-            />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for="event-name">Telefone</label>
-            <input
-              type="text"
-              class="form-control"
-              v-model="form.phone_number"
-              id="Sponsor-name"
-              placeholder="999 999 999"
-              :disabled="isEditable"
-            />
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="form-group">
-            <label for>Detalhes</label>
-            <div class="form-group">
-              <textarea class="form-control" rows="4" placeholder="Descrição do evento" :disabled="isEditable"></textarea>
-            </div>
-          </div>
-        </div>
-        <!-- <NextInput placeholder="Telefone, Email, Facebook, Instagram" /> -->
-      </div>
-      <div class="row flex">
-        <Address></Address>
       </div>
       <div class="row">
         <UploadPhoto
@@ -136,6 +52,7 @@
           :OnChange="SelectImage"
           width="140px"
           height="185px"
+          title="Logo"
         />
       </div>
       <div class="panel-footer">
@@ -146,7 +63,7 @@
       <p class="my-4">Tem certeza que deseja remover?</p>
       <template v-slot:modal-footer>
         <div class="w-100">
-          <b-button variant="primary" size="sm" class="float-right" @click="RemoveOrganizer">
+          <b-button variant="primary" size="sm" class="float-right" @click="RemoveCompany">
             <span v-if="!isRequesting">Sim</span>
             <div class="loading-dots" v-if="isRequesting">
               <div class="loading-dots--dot"></div>
@@ -166,14 +83,10 @@
   </div>
 </template>
 <script>
-// import NextInput from '@/components/Form/NextInput'
-import Address from '@/components/Form/Address'
 import UploadPhoto from '@/components/Form/Photo'
 
 export default {
   components: {
-    // NextInput,
-    Address,
     UploadPhoto
   },
   props: {
@@ -188,11 +101,10 @@ export default {
   data () {
     return {
       form: {},
-      companies: [],
       isRequesting: false,
+      hadSuccess: '',
       isOrderSaved: false,
       hadError: '',
-      hadSuccess: '',
       file: ''
     }
   },
@@ -205,32 +117,19 @@ export default {
     async ProcessForm () {
       this.hadError = ''
       const result = await this.$validator.validateAll()
-      return result ? this.UpdateOrganizer() : result
+      return result ? this.UpdateCompany() : result
     },
-    editOrganizer () {
+    editCompany () {
       this.isEditable = false
-    },
-    async allCompanies () {
-      try {
-        const result = await this.axios.get(`/companies/pages`)
-        const res = result.data
-        this.companies = res.data
-      } catch (e) {
-        this.hadError =
-          'Não foi possível carregar as encomendas. Actualize a página.'
-      }
-    },
-    SelectImage (file) {
-      this.file = file
     },
     /**
      * GetCompany: This method will fire a GET request and then
      * assign the response data into the state property: form
      */
-    async GetOrganizer () {
+    async GetCompany () {
       this.isRequesting = true
       try {
-        const result = await this.axios.get(`/organizer/${this.id}`)
+        const result = await this.axios.get(`/companies/${this.id}`)
         this.form = result.data
       } catch (e) {
         this.hadError = 'Não foi possível carregar as informações.'
@@ -241,12 +140,12 @@ export default {
      * GetCompany: This method will fire a GET request and then
      * assign the response data into the state property: form
      */
-    async RemoveOrganizer () {
+    async RemoveCompany () {
       this.isRequesting = true
 
       try {
         // Redirect to the Organizer views
-        await this.axios.delete(`/organizer/${this.id}`)
+        await this.axios.delete(`/companies/${this.id}`)
         location.reload()
       } catch (e) {
         this.hadError = 'Não foi possível efetuar esta operação.'
@@ -256,10 +155,10 @@ export default {
     /**
      * UpdateCompany: This method will send form to serve, for update
      */
-    async UpdateOrganizer () {
+    async UpdateCompany () {
       this.isRequesting = true
       try {
-        await this.axios.put(`/organizer/${this.id}`, this.form)
+        await this.axios.put(`/companies/${this.id}`, this.form)
         // this.$router.push({ name: "ListAddresses" });
         this.hadSuccess = 'Informações actualizadas com sucesso.'
       } catch (e) {
@@ -274,11 +173,13 @@ export default {
     hideRemoveModal () {
       // Show modal for deatils
       this.$bvModal.hide('modal-remove')
+    },
+    SelectImage (file) {
+      this.file = file
     }
   },
   created () {
-    this.allCompanies()
-    this.GetOrganizer()
+    this.GetCompany()
   }
 }
 </script>
@@ -302,4 +203,5 @@ export default {
   margin-top: 40px;
   height: 40px;
 }
+
 </style>
