a213e625b9d84471d4c2daa1e24d875aed12874f
Aristóteles Lopes <aristoteles.lopes@nextbss.co.ao>
2019-11-15 16:43:09 +0100

    global functions

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Functions from './store/helpers/functions'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    Functions
+  },
   state: {
     tableDetailID: String
   },
