0e1953550e6b3f393e14e884f9e8761ed3a93bd8
Aristóteles Lopes <aristoteles.lopes@nextbss.co.ao>
2019-11-15 16:48:38 +0100

    show more columns

--- src/views/Organizers/ListOrganizer.vue
+++ src/views/Organizers/ListOrganizer.vue
@@ -7,13 +7,17 @@
       <div>
         <Table
         :cols="cols"
-        :data="companies"
+        :data="organizers"
         title="Organizador"
-        :searchMethod="GetPosts"
-        :detailMethod="GetCompany"
+        :searchMethod="GetOrganizers"
+        :pagination="pagination"
+        :paginationMethod="GetOrganizers"
+        :sortMethod="GetOrganizers"
         :needGrid="true"
-        resource="company"
+        :changePage="changePage"
+        resource="organizer"
         editRoute="EditOrganizer"
+        :pageCount="pageCount"
         />
       </div>
       <div class="panel-footer">
@@ -31,41 +35,106 @@ export default {
     return {
       form: {},
       cols: [
-        { name: 'name', label: 'Nome' }
+        { name: 'name', label: 'Nome' },
+        { name: 'email', label: 'Email' },
+        { name: 'facebook', label: 'Facebook' },
+        { name: 'instagram', label: 'Instagram' },
+        { name: 'twitter', label: 'Twitter' },
+        { name: 'phone_number', label: 'Telefone' }
       ],
       isRequesting: false,
-      companies: [],
+      organizers: [],
+      pagination: {
+        perPage: 14,
+        pageable: { pageNumber: 1 }
+      },
+      data: [
+        {
+          id: 1,
+          name: 'Spider Man',
+          img: 'http://cdn.collider.com/wp-content/uploads/2019/05/spider-man-far-from-home-poster-fury-mysterio-2.jpg',
+          status: 'Available'
+        },
+        {
+          id: 2,
+          name: 'Avangers',
+          img: 'http://br.web.img3.acsta.net/r_1920_1080/pictures/19/04/26/17/30/2428965.jpg',
+          status: 'Pending'
+        },
+        {
+          id: 3,
+          name: 'Man in Black',
+          img: 'https://m.media-amazon.com/images/I/51jcUqVa4EL._SL500_.jpg',
+          status: 'Canceled'
+        },
+        {
+          id: 4,
+          name: 'Ana',
+          img: 'https://filmspot.com.pt/images/filmes/posters/big/484641_pt.jpg',
+          status: 'Available'
+        },
+        {
+          id: 5,
+          name: 'Simba',
+          img: 'https://filmspot.com.pt/images/filmes/posters/big/420818_pt.jpg',
+          status: 'Available'
+        }
+      ],
       hadError: '',
       hadSuccess: '',
-      editID: ''
+      editID: '',
+      pageCount: 0
     }
   },
   methods: {
     async GetPosts () {
       // eslint-disable-next-line no-unused-expressions
-      this.companies
+      this.organizers
     },
     /*
      *  GetCompanies: This method will fire a GET request
      *  to fetch the companies and the will store the result
      *  into the orders local state property
      */
-    async GetCompanies () {
+    async GetOrganizers (type, sort = '', search = '') {
       this.isRequesting = true
+
+      if (type === 'next') {
+        this.pagination.pageable.pageNumber += 1
+      }
+
+      if (type === 'prev') {
+        this.pagination.pageable.pageNumber -= 1
+      }
+
+      // API query options like: sorts and pagination
+      let query = ''
+      query += `pageNumber=${this.pagination.pageable.pageNumber}`
+      query += `&pageSize=${this.pagination.perPage}`
+      // query += sort ? `&sortBy=${sort}` : ''
+      query += search ? `&search=${search}` : ''
+
       try {
-        const result = await this.axios.get(`/companies/pages`)
+        const result = await this.axios.get(`/organizers?${query}`)
         const res = result.data
-        this.companies = res.data
+        this.organizers = res.data
+
+        this.pageCount = res.pages_count
+        // Set Pagination
+        // delete res.data.content
       } catch (e) {
         this.hadError =
           'Não foi possível carregar as encomendas. Actualize a página.'
       }
       this.isRequesting = false
+    },
+    changePage (page) {
+      this.pagination.pageable.pageNumber = page
     }
   },
   created () {
     // Get customer orders
-    this.GetCompanies()
+    this.GetOrganizers()
   }
 }
 </script>
@@ -90,9 +159,9 @@ export default {
   height: 40px;
 }
 label{
-    float: left !important;
+  float: left !important;
 }
 .modal-content{
-    margin-top: 15% !important;
+  margin-top: 15% !important;
 }
 </style>
