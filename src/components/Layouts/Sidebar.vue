d080e1abf485b19c8fd696c4eec485f72ceccda3
Aristóteles Lopes <aristoteles.lopes@nextbss.co.ao>
2019-12-10 16:12:14 +0100

    change link on sidebar component

--- src/components/Layouts/Sidebar.vue
+++ src/components/Layouts/Sidebar.vue
@@ -17,16 +17,9 @@
     <router-link :to="{name: 'ListCompany'}" tag="a">
       <Collapse title="Empresas"></Collapse>
     </router-link>
-    <Collapse title="Organizadores" >
-      <router-link :to="{name: 'ListOrganizer'}" tag="a">
-        <span>Listar </span>
-      </router-link>
-      <Collapse title="Operadores" :moreClass="true">
-          <router-link :to="{name: 'ListSupplier'}" tag="a">
-            <span>Listar</span>
-          </router-link>
-      </Collapse>
-    </Collapse>
+    <router-link :to="{name: 'ListOrganizer'}" tag="a">
+      <Collapse title="Organizadores" ></Collapse>
+    </router-link>
     <router-link :to="{name: 'ListSponsor'}" tag="a">
       <Collapse title="Patrocinadores">
           <span>Listar</span>
@@ -45,6 +38,9 @@
     </router-link>
     <router-link :to="{name: 'ListProduct'}" tag="a">
       <Collapse title="Produtos"></Collapse>
+    </router-link>
+    <router-link :to="{name: 'ListSupplier'}" tag="a">
+      <Collapse title="Operadores" ></Collapse>
     </router-link>
     <ul class="list-unstyled CTAs">
       <li @click="DoLogout" >
