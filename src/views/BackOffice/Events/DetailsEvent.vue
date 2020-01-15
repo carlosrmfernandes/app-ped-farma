a8753d5e45e90d85cfb269ce0df68a1c793507de
Nelson Mfinda <nelson.mfinda@nextbss.co.ao>
2020-01-15 03:41:58 -0500

    Implement list of event details

--- src/views/BackOffice/Events/DetailsEvent.vue
+++ src/views/BackOffice/Events/DetailsEvent.vue
@@ -247,6 +247,50 @@
           buttonRegistName = "Adicionar Sessão"
           :canRemove = false
         />
+        <div class="row">
+          <div class="col-md-6">
+            <p>Patrocinadores</p>
+            <MiniTable
+              :cols="suplier_cols"
+              :data="supliers"
+              title="Supliers"
+              :searchMethod="getEventSponsors"
+              :pagination="pagination"
+              :paginationMethod="getEventSponsors"
+              :sortMethod="getEventSponsors"
+              :needGrid="false"
+              :changePage="changePage"
+              resource="supliers"
+              editRoute="EditSuplier"
+              :pageCount="pageCount"
+              :removeResource="removeSession"
+              registRoute="RegistSuplier"
+              buttonRegistName = "Lista de Patrocinadores"
+              :canRemove = false
+            />
+          </div>
+          <div class="col-md-6">
+            <p>Decks</p>
+            <MiniTable
+              :cols="deck_cols"
+              :data="decks"
+              title="Decks"
+              :searchMethod="getEventDecks"
+              :pagination="pagination"
+              :paginationMethod="getEventDecks"
+              :sortMethod="getEventDecks"
+              :needGrid="false"
+              :changePage="changePage"
+              resource="decks"
+              editRoute="EditDeck"
+              :pageCount="pageCount"
+              :removeResource="removeSession"
+              registRoute="RegistSession"
+              buttonRegistName = "Lista de Decks"
+              :canRemove = false
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -283,6 +327,21 @@ export default {
         poster_path: '',
         backdrop_path: ''
       },
+      deck_cols: [
+        { name: 'name', label: 'Nome' },
+        { name: 'number', label: 'Número' },
+        { name: 'orientation', label: 'Orientação' },
+        { name: 'status', label: 'Estado' },
+        { name: 'starts_at', label: 'Data de Inicio' },
+        { name: 'ends_at', label: 'Data de Termino' }
+      ],
+      decks: [],
+      suplier_cols: [
+        { name: 'name', label: 'Nome' },
+        { name: 'email', label: 'Email' },
+        { name: 'phone_number', label: 'Telefone' }
+      ],
+      supliers: [],
       cols: [
         { name: 'type', label: 'Tipo' },
         { name: 'price', label: 'Preço' },
@@ -292,7 +351,6 @@ export default {
         { name: 'ends_at', label: 'Data de Termino' }
       ],
       isRequesting: false,
-      decks: [],
       pagination: {
         perPage: 12,
         pageable: { pageNumber: 1 }
@@ -440,6 +498,45 @@ export default {
         this.hadError = 'Não foi possível carregar as informações.'
       }
     },
+    async getEventDecks () {
+      try {
+        const result = await this.axios.get(`/decks?eventIds=${this.party_event_id}`)
+        const res = result.data
+        const itemsCount = result.data.items_count
+
+        for (let i = 0; i < itemsCount; i++) {
+          this.decks.push({
+            id: res.data[i].id,
+            name: res.data[i].name,
+            number: res.data[i].number,
+            orientation: res.data[i].orientation,
+            status: res.data[i].status,
+            starts_at: res.data[i].starts_at,
+            ends_at: res.data[i].ends_at
+          })
+        }
+      } catch (e) {
+        this.hadError = 'Não foi possível carregar as informações.'
+      }
+    },
+    async getEventSponsors () {
+      try {
+        const result = await this.axios.get(`/sponsors?eventIds=${this.party_event_id}`)
+        const res = result.data
+        const itemsCount = result.data.items_count
+
+        for (let i = 0; i < itemsCount; i++) {
+          this.supliers.push({
+            id: res.data[i].id,
+            name: res.data[i].name,
+            email: res.data[i].email,
+            phone_number: res.data[i].phone_number
+          })
+        }
+      } catch (e) {
+        this.hadError = 'Não foi possível carregar as informações.'
+      }
+    },
     async updatePartyEvent () {
       this.isRequesting = true
       try {
@@ -478,6 +575,8 @@ export default {
     this.getOrganizers()
     this.getLocations()
     this.getEvent()
+    this.getEventDecks()
+    this.getEventSponsors()
     this.getEventSessions()
   }
 }
